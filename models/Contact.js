371cc721261f1b38c1ff287d2e2e64559b95337c
MuppanaPavanDurga <muppana.pavandurga@gmail.com>
2026-05-28 17:38:43 +0530

    Update Contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -1,7 +1,4 @@
-import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/Testing")
-    .then(()=>console.log("MongoDB Server Connected"))
-    .catch((err)=>console.log("MongoDB COnnection Error : ",err))
+
 const ContactSchema=new mongoose.Schema(
     {
         email : {
